adcfaf0e9c34e36ee8898e5f9dd9515f28c7e8f7
Adam Silverman <adam@silverboxdev.com>
2014-08-08 13:59:23 -0500

    Added Typekit Support

--- wp-content/themes/ensign_custom/functions.php
+++ wp-content/themes/ensign_custom/functions.php
@@ -166,6 +166,22 @@ function wptuts_scripts_with_jquery()
 
 add_action( 'wp_enqueue_scripts', 'wptuts_scripts_with_jquery' );
 
+/**
+ * TypeKit Fonts
+ *
+ * @since Theme 1.0
+ */
+function theme_typekit() {
+    wp_enqueue_script( 'theme_typekit', '//use.typekit.net/tsk0zxz.js');
+}
+add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+function theme_typekit_inline() {
+  if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+  	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+}
+add_action( 'wp_head', 'theme_typekit_inline' );
+
 /***********************************************************************************/
 /* Silverbox Image */
 /***********************************************************************************/
