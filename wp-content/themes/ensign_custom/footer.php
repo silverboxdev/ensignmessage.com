8ec52ac4e4f247325e8b66d30ec01754dcfb6f48
Adam Silverman <adam@silverboxdev.com>
2014-08-11 14:04:53 -0500

    Spacing issues in header and footer

--- wp-content/themes/ensign_custom/footer.php
+++ wp-content/themes/ensign_custom/footer.php
@@ -1,7 +1,8 @@
-
+<?php if(!is_front_page()) { ?>
 <div class="sidebar_bottom_footer">
 	<a href="<?= get_page_link('21'); ?>" class="button dark_blue_button">Archives</a>
 </div>
+<?php } ?>
 
 <footer class="footer">
 	
