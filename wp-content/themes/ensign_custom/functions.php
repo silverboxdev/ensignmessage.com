9fc47e79ee1d83727b50551982d25adaf612e913
Adam Silverman <adam@silverboxdev.com>
2014-08-13 11:38:35 -0500

    Fixes to functions file for PWP posts. Updates to CSS for submit buttons on the site

--- wp-content/themes/ensign_custom/functions.php
+++ wp-content/themes/ensign_custom/functions.php
@@ -135,6 +135,26 @@ function new_excerpt_more($more) {
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
+
+function my_excerpt_protected( $excerpt ) {
+    if ( post_password_required() )
+        $excerpt = '<p>This article is password protected. If you are subscriber <a href="/contact">contact</a> the editor to receive your password.  If not, <a href="/subscriptions">subscribe</a> today.</p>';
+    return $excerpt;
+}
+add_filter( 'the_excerpt', 'my_excerpt_protected' );
+
+function my_password_form() {
+    global $post;
+    $label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
+    $o = '<form action="' . esc_url( site_url( 'wp-login.php?action=postpass', 'login_post' ) ) . '" method="post">
+    ' . __( 'This article is password protected. To view it please enter your password below. If you are subscriber <a href="/contact">contact</a> the editor to receive your password.  If not, <a href="/subscriptions">subscribe</a> today.</p>') . '
+   <p><label for="' . $label . '">' . __( "Password:" ) . ' </label><input name="post_password" id="' . $label . '" type="password" size="20" maxlength="20" /><input type="submit" name="Submit" value="' . esc_attr__( "Submit" ) . '" class="button blue_button" />
+    </p></form>
+    ';
+    return $o;
+}
+add_filter( 'the_password_form', 'my_password_form' );
+
 /***********************************************************************************/
 /* Enqueue Styles */
 /***********************************************************************************/
