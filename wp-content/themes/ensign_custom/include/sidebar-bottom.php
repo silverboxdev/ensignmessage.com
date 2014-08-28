259e26e72e277a6c2dcf8059459a2d2ef4f27a33
Adam Silverman <adam@silverboxdev.com>
2014-08-28 10:40:01 -0500

    Added Donate button to footer

--- wp-content/themes/ensign_custom/include/sidebar-bottom.php
+++ wp-content/themes/ensign_custom/include/sidebar-bottom.php
@@ -1,3 +1,6 @@
 <div class="sidebar_bottom">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="margin: 1em 0;"><input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="UPTB2445UX67S"><input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal Ð The safer, easier way to pay online."><img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
 	<a href="<?= get_page_link('21'); ?>" class="button dark_blue_button">Archives</a>
 </div>
