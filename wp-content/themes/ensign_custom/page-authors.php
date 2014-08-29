dbc704cad3bca83bfed1550c164730406bc2d3be
Adam Silverman <adam@silverboxdev.com>
2014-08-29 13:52:56 -0500

    Authors template added

--- wp-content/themes/ensign_custom/page-authors.php
+++ wp-content/themes/ensign_custom/page-authors.php
@@ -39,30 +39,26 @@
 			<h1 class="pg_title">Author Archives</h1>
 			
 			<ul class="archive_list">
-				<?php
-				global $wpdb;
-				$limit = 0;
-				$year_prev = null;
-				$months = $wpdb->get_results("SELECT DISTINCT MONTH( post_date ) AS month ,	YEAR( post_date ) AS year, COUNT( id ) as post_count FROM $wpdb->posts WHERE post_status = 'publish' and post_date <= now( ) and post_type = 'post' GROUP BY month , year ORDER BY post_date DESC");
-				foreach($months as $month) :
-					$year_current = $month->year;
-					if ($year_current != $year_prev){
-						if ($year_prev != null){?>
-						
-						<?php } ?>
-					
-					<hr />
-					<li class="archive-year"><h2><a href="<?php bloginfo('url') ?>/articles/<?php echo $month->year; ?>/"><?php echo $month->year; ?></a></h2></li>
-					
-					<?php } ?>
-					<li class="archive-month"><a href="<?php bloginfo('url') ?>/articles/<?php echo $month->year; ?>/<?php echo date("m", mktime(0, 0, 0, $month->month, 1, $month->year)) ?>"><span class="archive-month"><?php echo date_i18n("F", mktime(0, 0, 0, $month->month, 1, $month->year)) ?></span></a></li>
-				<?php $year_prev = $year_current;
-				
-				if(++$limit >= 18) { break; }
-				
-				endforeach; ?>
-			</ul>
-				
+				<?php $args = array(
+				    'orderby'       => 'name', 
+				    'order'         => 'ASC', 
+				    'number'        => null,
+				    'optioncount'   => true, 
+				    'exclude_admin' => true, 
+				    'show_fullname' => true,
+				    'hide_empty'    => true,
+				    'echo'          => true,
+				    'feed_image'    =>'',
+				    'feed_type'     => '',
+				    'style'         => 'list',
+				    'html'          => true,
+				    'exclude'       =>'',
+				    'include'       => '');
+				 
+					 // The Query
+					 wp_list_authors($args);
+				?>
+			</ul>			
 		</article>	
 		
 		<!-- Sidebar Bottom -->
