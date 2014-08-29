57537c5f67cb4a83b28028b281afe88fae90c5b5
Adam Silverman <adam@silverboxdev.com>
2014-08-29 14:20:36 -0500

    Function added for removing the word 'protected' from the title

--- wp-content/themes/ensign_custom/functions.php
+++ wp-content/themes/ensign_custom/functions.php
@@ -155,6 +155,21 @@ function my_password_form() {
 }
 add_filter( 'the_password_form', 'my_password_form' );
 
+function the_title_trim($title) {
+	$title = attribute_escape($title);
+	$findthese = array(
+		'#Protected:#',
+		'#Private:#'
+	);
+	$replacewith = array(
+		'', // What to replace "Protected:" with
+		'' // What to replace "Private:" with
+	);
+	$title = preg_replace($findthese, $replacewith, $title);
+	return $title;
+}
+add_filter('the_title', 'the_title_trim');
+
 /***********************************************************************************/
 /* Enqueue Styles */
 /***********************************************************************************/
