a514d39f4829e246c8d2079ff9ea43209373a775
Adam Silverman <adam@silverboxdev.com>
2014-09-12 11:16:57 -0500

    fixed parse error in PHP

--- wp-content/themes/ensign_custom/posts.php
+++ wp-content/themes/ensign_custom/posts.php
@@ -2,7 +2,7 @@
 	
 	<div class="f_copy">
 	
-		<h1><a href="<?[ the_permalink(); ?>" class="post_title"><?= the_title(); ?></a></h1>
+		<h1><a href="<?php the_permalink(); ?>" class="post_title"><?= the_title(); ?></a></h1>
 		
 		<p class="author_meta">By <?= the_author_posts_link(); ?></p>
 		
